9bf033244cdeb4a6c1c0f211ad1cf43d0a8c7ba9
MohammadHadi Keshavarzi <m.keshavarzi@aut.ac.ir>
2021-12-10 01:04:40 +0330

    start project

---
+++ src/Profile.java
@@ -0,0 +1,21 @@
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.math.BigInteger;
+public class Profile {
+    private String firstName;
+    private String lastName;
+    private String bio;
+    final private String username;
+    final private LocalDateTime registerDate;
+    private BigInteger password;
+
+    public Profile(String firstName,String lastName,String username,String notCoded_Pass) throws NoSuchAlgorithmException{
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.username=username;
+        MessageDigest msd= MessageDigest.getInstance("SHA-256");
+        this.password=new BigInteger(1,msd.digest(notCoded_Pass.getBytes(StandardCharsets.UTF_8)));
+    }
+}
